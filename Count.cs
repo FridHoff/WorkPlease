d45d4b865660e224ac221eb16982283b2b2b8f74
FridHoff <flex210879@mail.ru>
2021-03-15 11:05:45 +0300

    Added constructor and fields to class

--- Count.cs
+++ Count.cs
@@ -8,5 +8,20 @@ namespace Rabotai
 {
     class Count
     {
+        string name;
+        int number;
+        double sum;
+        Count()
+        {
+            this.name = "Счёт";
+            this.number = 1;
+            this.sum = 0;
+        }
+        Count(string cName, int cNumber, double cSum)
+        {
+            this.name = cName;
+            this.number = cNumber;
+            this.sum = cSum;
+        }
     }
 }
